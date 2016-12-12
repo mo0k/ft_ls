02f35973612e3f866fc7e5737b90f48580c05727
Julien MOUCADEL <jmoucade@e3r4p14.42.fr>
2016-12-12 04:57:01 +0100

    add filestat

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: jmoucade <jmoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/10 20:53:17 by jmoucade          #+#    #+#             */
-/*   Updated: 2016/12/11 06:57:10 by jmoucade         ###   ########.fr       */
+/*   Updated: 2016/12/12 00:40:35 by jmoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@
 
 #include <libft.h>
 
-# define USAGE ft_putstr("usage: ./ft_ls [-alRr] [file ...]\n");
+# define USAGE ft_putstr("usage: ./ft_ls [-Ralrt1] [file ...]\n");
 
 typedef struct	s_lst_file
 {
@@ -38,6 +38,7 @@ typedef struct	s_lst_file
 	char				*date;
 	char				*name;
 	struct s_lst_file 	*next;
+	struct s_lst_file 	*prev;
 }						t_lst_file;
 
 typedef struct			s_lst_input
@@ -47,29 +48,29 @@ typedef struct			s_lst_input
 	struct s_lst_input	*next;
 }						t_lst_input;
 
-
 #include <padding.h>
-
-char					*verif_filepath(char *path);
 
 char 					*convert_permi(char nbr);
 char 					*get_permi(char *nbr, mode_t	m);
 char 					get_filetype(mode_t	m);
-int 					browse_dir(char *path, t_lst_file **lst);
-
 int 					file_stat(char *filename, t_lst_file **lst);
-t_lst_file				*add_lst_file(t_lst_file **f);
+int 					browse_dir(char *path, t_lst_file **lst);
+char					*verif_filepath(char *path);
+
+t_lst_file				*add_lst_file(t_lst_file **f, t_lst_file *prev);
 t_lst_input				*add_lst_input(t_lst_input **f);
 void					del_lst_file(t_lst_file **f);
 void					del_lst_input(t_lst_input **l);
+void					del_one(t_lst_file **l, t_lst_file *del);
 void 					print_lst_file(t_lst_file *f);
 void 					print_lst_input(t_lst_input *f);
+int						lst_sort(t_lst_file **l);
+void					swap_element(t_lst_file **elem1, t_lst_file **elem2);
 
 t_lst_input				*check_input(int ac, char **av, char *all_options, char **options);
 int						is_options(char const *str);
 int						is_option_valid(char const *str, char *all_options);
 int						stock_options(char **options, char const *str);
 int 					stock_filepath(t_lst_input **lst, char const *str);
-
 
 #endif
