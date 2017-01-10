09f6038d5896250708f594177c940b77c3c872d2
Julien MOUCADEL <jmoucade@e2r4p2.42.fr>
2017-01-10 03:00:58 +0100

    modif headers

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: jmoucade <jmoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/10 20:53:17 by jmoucade          #+#    #+#             */
-/*   Updated: 2016/12/12 00:40:35 by jmoucade         ###   ########.fr       */
+/*   Updated: 2017/01/10 02:59:00 by jmoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int				main(int ac, char **av)
 {
 	t_opts		*opts;
 	t_lstall	*lst_root;
-	t_lstall	*tmp;
+	//t_lstall	*tmp;
 	t_sfunc		sfunc;
 	t_pfunc		pfunc;
 
@@ -28,14 +28,16 @@ int				main(int ac, char **av)
 	pfunc = get_pfunc(opts);
 	sort(&lst_root->dir, opts, sfunc);
 	sort(&lst_root->file, opts, sfunc);
-	tmp = lst_root;
+	//tmp = lst_root;
 	print_root_file(lst_root->file, opts, pfunc);
 	if (lst_root->file && lst_root->dir)
 		write(1, "\n", 1);
-	if (!(browse_first_node(&tmp->dir, opts, pfunc, sfunc)))
+	//if (!(browse_first_node(&tmp->dir, opts, pfunc, sfunc)))
+	if (!(browse_first_node(&lst_root->dir, opts, pfunc, sfunc)))
 		return (1);
 	free(opts);
 	free(lst_root);
+	while(1){};
 	return (0);
 }
 
